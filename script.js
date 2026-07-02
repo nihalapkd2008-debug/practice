660f801bda245e9066d292eebe502ea39e7213eb
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-07-02 21:11:42 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const user = {
-    name: "Nihala",
-    age: 18,
-    place: "Kannur",
-    course: "JavaScript"
+name: "nihala",
+age: 18,
+course: "JavaScript",
+Place: "kannur"
 };
-
 console.log(user);
+
+
+
+const items = [
+    {
+        products: "laptop",
+        price: 200,
+        name: "dell",
+        
+       brand: "toyota",
+       rate: 2000000,
+       name: "Benz",
+
+    brand: "nano",
+       rate: 200000,
+       name: "alto",
+
+
+    }
+]
+let name = "niha",
+function showName(name) {
+
+    console.log(name);
+}
+showName()
