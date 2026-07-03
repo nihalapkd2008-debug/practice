5d8c909b56cf3b6432b4d552f50787c4c93c6c10
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-07-03 20:31:16 +0530

    updated

---
+++ script.js
@@ -0,0 +1,24 @@
+const productContainer = document.getElementById("products");
+async function getProducts(){
+const response = await fetch("https://fakestoreapi.com/products");
+const products = await response.json();
+products.forEach(function(products) {
+
+  productContainer.innerHTML+=`
+  <div class="card">
+  <img src="${products.image}">
+
+        <h3>${product.title}</h3>
+
+        <p>₹ ${product.price}</p>
+
+        <p>${product.category}</p>
+
+        <button>View Details</button>
+
+        </div>
+  
+  `;
+});
+}
+getProducts();
